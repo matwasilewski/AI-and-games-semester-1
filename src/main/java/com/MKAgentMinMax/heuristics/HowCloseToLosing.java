de2a4938f95368619cfe088ce0669476f42f204d
santiagonasar <mat.p.wasilewski@gmail.com>
2020-12-18 01:35:43 +0100

    add heuristics calculating score on basis of how close the player and enemy are to winning / losing

---
+++ src/main/java/com/MKAgentMinMax/heuristics/HowCloseToLosing.java
@@ -0,0 +1,21 @@
+package com.MKAgentMinMax.heuristics;
+
+import com.MKAgent.Board;
+
+public class HowCloseToLosing extends Heuristic {
+
+    public HowCloseToLosing(Integer weight) {
+        super(weight);
+    }
+
+    /**
+     * should return the score based on heuristics.
+     *
+     * @param board     The board to check the game state for.
+     * @return If agent player is winning result > 0 else result < 0
+     */
+    @Override
+    public Integer getScore(Board board) {
+        return board.getNoOfSeeds() - board.getSeedsInStore(board.getAgentsSide().opposite());
+    }
+}
